9bc2b08397805c7c0172064cce636f3032fa06a7
Saleem-un-qayoom <saleemunqayoom@gmail.com>
2022-05-31 18:22:04 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Project1 from './View/Project1/Project1';
+
+function App() {
+	return (
+		<div>
+			<Project1 />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/View/Project1/Project1.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Project1() {
+	return <div className="project-1"></div>;
+}
+
+export default Project1;
